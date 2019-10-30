0abed74d287b11ebbfc84c2c1c889cb4068d3390
Peter <hguanqi7@gmail.com>
2019-10-30 14:11:19 +1300

    Enemy Base mod0

--- Assets/EnemyStuff/EnemyBase.cs
+++ Assets/EnemyStuff/EnemyBase.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+    //Enemy Stats
+    //Change public to private later
+    public float maxHitPoint;
+    public float hitPoint;
+    public float speed;
+    public float dmg;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hitPoint = maxHitPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void AImove()
+    {
+
+    }
+
+    void DoMove()
+    {
+
     }
 }
