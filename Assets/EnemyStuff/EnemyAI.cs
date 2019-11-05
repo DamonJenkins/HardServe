e4af85af9361c3b9a1ae76405370a17a81fceb5d
Peter <hguanqi7@gmail.com>
2019-11-05 21:48:36 +1300

    AI tracking player

--- Assets/EnemyStuff/EnemyAI.cs
+++ Assets/EnemyStuff/EnemyAI.cs
@@ -6,14 +6,14 @@ using Pathfinding;
 public class EnemyAI : MonoBehaviour
 {
     public Transform target;
-
+    public AstarPath AstarObj;
     public float speed;
     public float nexWayPointDistance = 3f;
 
     //Path the AI following
     Path path;
     int currentWaypoint = 0;
-    bool reachedEndOfPath = false;
+    bool reachedEnd = false;
 
     Seeker seeker;
     Rigidbody2D rb;
@@ -21,11 +21,60 @@ public class EnemyAI : MonoBehaviour
     void Start()
     {
         
+        
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePathGen",0f,0.5f);
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    //func to keep updating A* path
+    void UpdatePathGen()
     {
-        
+        if (seeker.IsDone())
+        {
+            AstarObj.Scan();
+            seeker.StartPath(rb.position, target.position, PathComplete);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEnd = true;
+            return;
+        }
+        else
+        {
+            reachedEnd = false;
+        }
+
+        Vector2 dir =((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = dir * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nexWayPointDistance)
+        {
+            currentWaypoint++;
+        }
+
+
+
+    }
+
+    void PathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 }
